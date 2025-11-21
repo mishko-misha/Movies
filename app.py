6a807a48780002380aa21223694d0b21c01750ef
Misha Polunin <polunin1986@gmail.com>
2025-11-21 16:00:20 +0200

    - created templates folder → created login.html,register.html, index.html (index its homepage)
     - remade home page'/', /login, /register (added Get, because we call login page but it impossible if we have only POST)

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
-    return "Movie Home Page!"
+    return render_template('index.html')
 
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def user_register():
-    return 'Register'
+    return render_template('register.html')
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET','POST'])
 def user_login():
-    return 'Login'
+    return render_template('login.html')
 
 
 @app.route('/logout', methods=['GET'])

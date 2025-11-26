7fd143f5f5f116cdf4961e50476d4f461057504f
Misha Polunin <polunin1986@gmail.com>
2025-11-26 14:20:06 +0200

    - Created database.db and added tables - app.py added sqlite connection for: 	- /, 	- /films, 	- /films/<film_id>, 	- /films/<film_id>/rating, 	- /films/<film_id>/rating/<feedback_id>/feedback

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask,render_template
+import sqlite3
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
-    return render_template('index.html')
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    cur.execute('SELECT name, year, country FROM film ORDER BY added_at DESC LIMIT 5')
+    result_films = cur.fetchall()
+    return render_template('index.html', films=result_films)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -35,12 +40,23 @@ def user_delete(user_id):
 
 @app.route('/films', methods=['GET', 'POST'])
 def films():
-    return 'Films'
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    cur.execute('SELECT id, poster, name FROM film ORDER BY added_at DESC')
+    result_films = cur.fetchall()
+    return f'Films {result_films}'
 
 
 @app.route('/films/<film_id>', methods=['GET', 'PUT'])
 def film_detail(film_id):
-    return f'Film {film_id}'
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    cur.execute('SELECT * FROM film WHERE id = ?', (film_id,)) # more secure with parameterized query
+    result = cur.fetchone()
+
+    actors = cur.execute(f'SELECT * FROM actor JOIN actor_film on actor.id == actor_film.actor_id WHERE actor_film.film_id = ?', (film_id,)).fetchall()
+    genres = cur.execute(f'SELECT * FROM genre_film WHERE film_id = ?', (film_id,)).fetchall()
+    return f'Film {result}, Actors: {actors}, Genres: {genres}'
 
 
 @app.route('/films/<film_id>', methods=['DELETE'])
@@ -50,12 +66,17 @@ def film_delete(film_id):
 
 @app.route('/films/<film_id>/rating', methods=['GET', 'POST'])
 def film_rating(film_id):
-    return f'Rating for Film {film_id}'
-
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    rating = cur.execute('SELECT id, rating FROM film WHERE id = ?', (film_id,)).fetchone()
+    return f'Rating for Film {film_id}: {rating}'
 
 @app.route('/films/<film_id>/rating/<feedback_id>', methods=['GET', 'POST'])
 def film_feedback(film_id, feedback_id):
-    return f'Feedback {feedback_id} for Film {film_id}'
+    con = sqlite3.connect('database.db')
+    cur = con.cursor()
+    feedback = cur.execute('SELECT film, grade, description FROM feedback WHERE film = ? AND id = ?' , (film_id,feedback_id)).fetchone()
+    return f'Feedback {feedback_id} for Film {film_id}: {feedback}'
 
 
 @app.route('/users/<user_id>/lists', methods=['GET', 'POST'])
@@ -79,4 +100,4 @@ def remove_film_from_list(user_id, list_id, film_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

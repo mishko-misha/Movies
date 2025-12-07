693a037d125cfe2dcc9ffd97b69b94c1baf32e61
Misha Polunin <polunin1986@gmail.com>
2025-12-07 14:41:35 +0200

    Templates: - created base.html as skeleton - created  header.html and added content - created user_profile.html and added form - edited index.html
    App.py
    - added secret_key,session, redirect, url_for
    - extended:
    	- main_page, user_login,user_profile,logout methods

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 
 from database_connection import DatabaseConnection
 
 app = Flask(__name__)
+app.secret_key = 'AS1214jds123%!@#'
 
 
 @app.route('/')
 def main_page():
+    user_session = session.get('logged_in', False)  # session as dict key: logged_in value: True/False
+    username = None  # Guarantee that variable exists
+    if user_session:
+        with DatabaseConnection() as db:
+            user = db.execute('SELECT * FROM user WHERE id = ?', (session['user_id'],)).fetchone()
+            if user:
+                username = user[
+                    'login']  # Get username from database and display it on main page in Hello block if user exists
+
+    # Collecting the 5 latest films added to the database
     with DatabaseConnection() as db:
-        result = db.execute('SELECT name, year, country FROM film ORDER BY added_at DESC LIMIT 5').fetchall()
-        return render_template('index.html', films=result)
+        list_of_films = db.execute('SELECT id,name, year, country FROM film ORDER BY added_at DESC LIMIT 5').fetchall()
+
+    return render_template('index.html', user_session=user_session, username=username, films=list_of_films)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -17,33 +29,72 @@ def user_register():
     if request.method == 'POST':
         first_name = request.form['username']
         last_name = request.form['lname']
+        login = request.form['login']
         email = request.form['email']
         password = request.form['password']
         birth_date = request.form['birth_date']
-        login = email  # Using email as login
         with DatabaseConnection() as db:
             db.execute(
-                'INSERT INTO user (first_name, last_name, login,email, password, birth_date) VALUES (?, ?, ?, ?, ?, ?)',
-                (first_name, last_name, login, email, password, birth_date))
+                'INSERT INTO user (first_name, last_name, password, login,email, birth_date) VALUES (?, ?, ?, ?, ?, ?)',
+                (first_name, last_name, password, login, email, birth_date))
     return render_template('register.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def user_login():
+    if request.method == 'POST':
+        login = request.form['login']
+        password = request.form['password']
+        with DatabaseConnection() as db:
+            user = db.execute('SELECT * FROM user WHERE login = ? AND password = ?', (login, password)).fetchone()
+            if user:
+                session['logged_in'] = True
+                session['user_id'] = user['id']
+                return redirect(url_for('main_page'))
     return render_template('login.html')
 
 
 @app.route('/logout', methods=['GET'])
 def user_logout():
-    return 'Logout'
+    session.clear()
+    return redirect(url_for('main_page'))
 
 
-@app.route('/users/<user_id>', methods=['GET', 'PUT'])
+@app.route('/users/<int:user_id>', methods=['GET', 'POST'])
 def user_profile(user_id):
-    return f'User {user_id}'
+    session_user_id = session.get('user_id')
+    if request.method == 'POST':
+        if user_id != session_user_id or session_user_id is None:
+            return 'Unauthorized', 403
+
+        first_name = request.form['username']
+        last_name = request.form['lname']
+        password = request.form['password']
+        email = request.form['email']
+        phone_number = request.form['phone_number']
+        birth_date = request.form['birth_date']
+        photo = request.form['photo']
+        additional_info = request.form['additional_info']
+        with DatabaseConnection() as db:
+            db.execute('''
+                UPDATE user 
+                SET first_name = ?, last_name = ?, password = ?, email = ?, phone_number = ?, birth_date = ?, photo = ?, additional_info = ? 
+                WHERE id = ?
+            ''', (first_name, last_name, password, email, phone_number, birth_date, photo, additional_info, user_id))
+        return 'Profile updated successfully'
+
+    else:
+        with DatabaseConnection() as db:
+            user = db.execute('SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
+            if user is None:
+                return 'User not found', 404
+
+            if session_user_id:
+                user_session = db.execute('SELECT * FROM user WHERE id = ?', (session_user_id,)).fetchone()
+            return render_template('user_profile.html', user=user, user_session=user_session)
 
 
-@app.route('/users/<user_id>', methods=['DELETE'])
+@app.route('/users/<user_id>/delete', methods=['DELETE'])
 def user_delete(user_id):
     return f'User {user_id}'
 
